0422ebb5c6b4f3536f5c6311cdc5eb1ac3145b1c
Layson Ndenza <laysonndenza@gmail.com>
2025-06-02 03:00:10 +0300

    Added the GlobalStyles.js file with the styled css

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,30 @@ const Button = styled.button`
   border-radius: 7px;
   background-color: purple;
   color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>The Wild Oasis</H1>
       <Button onClick={() => alert("Check in")}>Check in</Button>
-    </div>
+      <Button onClick={() => alert("Check Out")}>Check out</Button>
+
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
   );
 }
 
